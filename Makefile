897199ad6c699f99829fc49a76deee907989b15c
idohalevi <ido.h@logz.io>
2019-09-01 12:46:40 +0300

    adding end to end tests

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	go build -buildmode=c-shared -o build/out_logzio.so ./output
+
+clean:
+	rm -rf *.so *.h *~
--- output/logger.go
+++ output/logger.go
@@ -19,13 +19,13 @@ func NewLogger(prefix string, debug bool) *Logger {
 // Debug prints logs only if debug flag is true
 func (l *Logger) Debug(message string) {
 	if l.debug {
-		log.Printf("[%s] %s\n", l.prefix, message)
+		log.Printf("[%s] %s", l.prefix, message)
 	}
 }
 
 // Log prints every log
 func (l *Logger) Log(message string) {
-	log.Printf("[%s] %s\n", l.prefix, message)
+	log.Printf("[%s] %s", l.prefix, message)
 }
 
 // SetDebug sets debug flag
